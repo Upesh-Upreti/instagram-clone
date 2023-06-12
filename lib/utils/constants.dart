a94049a17137fe7611948ad5433c3ac72eb96ecf
upesh-upreti <upeshupreti@gmail.com>
2023-06-12 20:18:37 +0545

    Creates the post card of home news feed

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -8,4 +8,7 @@ class Constants {
   static const String ADD_OUTLINED = 'assets/vectors/add-square-outline.svg';
   static const String REELS_BOLD = 'assets/vectors/reels-bold.svg';
   static const String REELS_OUTLINED = 'assets/vectors/reels-outline.svg';
+
+  //Avatar image
+  static const String AVATAR_IMAGE = 'assets/images/Captain_America.png';
 }
--- lib/widgets/home_widgets/post_layout.dart
+++ lib/widgets/home_widgets/post_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/constants.dart';
 
 class PostLayout extends StatefulWidget {
   const PostLayout({super.key});
@@ -25,12 +26,132 @@ class _PostLayoutState extends State<PostLayout> {
       itemBuilder: ((context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(
-            horizontal: 8,
-            vertical: 10,
+            horizontal: 5,
+            vertical: 5,
           ),
           child: Container(
-            height: 500,
-            color: Colors.red,
+            height: 563,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 8,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        backgroundImage: AssetImage(Constants.AVATAR_IMAGE),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        'Captain America',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {
+                          print('menu pressed');
+                        },
+                        icon: Icon(Icons.more_vert),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Image.asset(
+                    Constants.AVATAR_IMAGE,
+                    fit: BoxFit.cover,
+                    height: 380,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.comment_outlined),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.share),
+                      ),
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.save),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    '346 Likes',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        "Captain America",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Flexible(
+                        child: Text(
+                          "Look out for each other. This is the fight of our lives.Whatever it takes. Good luck.",
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    "View all 128 comments",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    "3 hours ago",
+                    style: TextStyle(fontSize: 10),
+                  ),
+                ],
+              ),
+            ),
           ),
         );
       }),
